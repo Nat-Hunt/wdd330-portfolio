df799a190e3124a7052d213bdd6c2542703a486a
Nat-Hunt <nathan.huntlds@gmail.com>
2022-09-20 11:03:42 -0500

    Updated Styles, added copyright section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-const links = [
+const linksBlock1 = [
     {
         label: "Week1 notes",
         url: "./week1/index.html"
     }
 ]
+// const linksBlock2 = [
+//     {
+//         label: "Week8 notes",
+//         url: "./week8/index.html"
+//     }
+// ]
 
-links.forEach(
+linksBlock1.forEach(
     item => {
         var label = item.label;
         var url = item.url;
-        var ol = document.getElementById("dynamicNavBar");
+        var ol = document.getElementById("dynamicNavBarBlock1");
         var li = document.createElement('li');
         var a = document.createElement('a');
         a.appendChild(document.createTextNode(label));
@@ -18,3 +24,25 @@ links.forEach(
         ol.appendChild(li);
     }
 )
+
+// linksBlock2.forEach(
+//     item => {
+//         var label = item.label;
+//         var url = item.url;
+//         var ol = document.getElementById("dynamicNavBarBlock1");
+//         var li = document.createElement('li');
+//         var a = document.createElement('a');
+//         a.appendChild(document.createTextNode(label));
+//         a.href = url;
+//         li.appendChild(a);
+//         ol.appendChild(li);
+//     }
+// )
+
+// add copywright info for footer
+const currentDate = new Date();
+const copyright = "&copy ";
+const name = " | Nathan T. Hunt | Alabama";
+document.querySelector('#year').innerHTML = copyright.concat(currentDate.getFullYear(), name);
+const lastUpdate = "Last Updated: ";
+document.querySelector('#last-update').textContent = lastUpdate.concat(document.lastModified);
